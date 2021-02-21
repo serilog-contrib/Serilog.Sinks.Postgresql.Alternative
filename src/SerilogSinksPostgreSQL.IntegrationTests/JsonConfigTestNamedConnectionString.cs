a7df0a6cab5e3ce6c6bdbab3501b84b351c0baa9
SeppPenner <HansM2013@gmx.de>
2021-02-21 17:22:45 +0100

    SerilogSinksPostgreSQL.IntegrationTests: Added tests for the named connection strings.

---
+++ src/SerilogSinksPostgreSQL.IntegrationTests/JsonConfigTestNamedConnectionString.cs
@@ -0,0 +1,109 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="JsonConfigTestNamedConnectionString.cs" company="Hämmer Electronics">
+// The project is licensed under the MIT license.
+// </copyright>
+// <summary>
+//   Tests for creating PostgreSql logger from a JSON configuration with named connection strings.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SerilogSinksPostgreSQL.IntegrationTests
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Microsoft.Extensions.Configuration;
+
+    using Serilog;
+
+    using SerilogSinksPostgreSQL.IntegrationTests.Objects;
+
+    using Xunit;
+
+    /// <summary>
+    /// Tests for creating PostgreSql logger from a JSON configuration with named connection strings.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    public class JsonConfigTestNamedConnectionString : BaseTests
+    {
+        /// <summary>
+        /// The test logs.
+        /// </summary>
+        private const string TableName = "TestLogs";
+
+        /// <summary>
+        /// The database helper.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here.")]
+        private readonly DbHelper dbHelper = new DbHelper(ConnectionString);
+
+        /// <summary>
+        ///     This method is used to test the logger creation from the configuration.
+        /// </summary>
+        [Fact]
+        public void ShouldCreateLoggerFromConfig()
+        {
+            this.dbHelper.RemoveTable(string.Empty, TableName);
+
+            var testObject = new TestObjectType1 { IntProp = 42, StringProp = "Test" };
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(".\\PostgreSinkConfigurationConnectionString.json", false, true)
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            const int RowsCount = 2;
+
+            for (var i = 0; i < RowsCount; i++)
+            {
+                logger.Information(
+                    "{@LogEvent} {TestProperty}",
+                    testObject,
+                    "TestValue");
+            }
+
+            logger.Dispose();
+
+            var actualRowsCount = this.dbHelper.GetTableRowsCount(string.Empty, TableName);
+
+            Assert.Equal(RowsCount, actualRowsCount);
+        }
+
+        /// <summary>
+        ///     This method is used to test the logger creation from the configuration with the level column as text.
+        /// </summary>
+        [Fact]
+        public void ShouldCreateLoggerFromConfigWithLevelAsText()
+        {
+            this.dbHelper.RemoveTable(string.Empty, TableName);
+
+            var testObject = new TestObjectType1 { IntProp = 42, StringProp = "Test" };
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(".\\PostgreSinkConfiguration.Level.json", false, true)
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            const int RowsCount = 2;
+
+            for (var i = 0; i < RowsCount; i++)
+            {
+                logger.Information(
+                    "{@LogEvent} {TestProperty}",
+                    testObject,
+                    "TestValue");
+            }
+
+            logger.Dispose();
+
+            var actualRowsCount = this.dbHelper.GetTableRowsCount(string.Empty, TableName);
+
+            Assert.Equal(RowsCount, actualRowsCount);
+        }
+    }
+}

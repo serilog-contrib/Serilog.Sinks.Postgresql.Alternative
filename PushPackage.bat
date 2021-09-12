e6957fda53b8942b6faa800d97f2d5ec7dddb6f4
SeppPenner <HansM2013@gmx.de>
2021-09-12 13:45:09 +0200

    Removed package push to my private packages (Don't exist anymore with project under the hood of serilog-contrib).

--- PushPackage.bat
+++ PushPackage.bat
@@ -1,3 +1,2 @@
-dotnet nuget push "src\Serilog.Sinks.Postgresql.Alternative\bin\Release\Serilog.Sinks.Postgresql.Alternative.*.nupkg" -s "github" --skip-duplicate
 dotnet nuget push "src\Serilog.Sinks.Postgresql.Alternative\bin\Release\Serilog.Sinks.Postgresql.Alternative.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
 PAUSE

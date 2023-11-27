7fe92f5cf61741a71492dd68d809ede1b40efde4
SeppPenner <HansM2013@gmx.de>
2023-11-27 21:46:14 +0100

    Serilog.Sinks.Postgresql.Alternative: Smaller changes.

--- src/Serilog.Sinks.Postgresql.Alternative/Sinks/PostgreSQL/ColumnOptions.cs
+++ src/Serilog.Sinks.Postgresql.Alternative/Sinks/PostgreSQL/ColumnOptions.cs
@@ -23,11 +23,11 @@ public static class ColumnOptions
     public static IDictionary<string, ColumnWriterBase> Default =>
         new Dictionary<string, ColumnWriterBase>
         {
-                { DefaultColumnNames.RenderedMessage, new RenderedMessageColumnWriter() },
-                { DefaultColumnNames.MessageTemplate, new MessageTemplateColumnWriter() },
-                { DefaultColumnNames.Level, new LevelColumnWriter() },
-                { DefaultColumnNames.Timestamp, new TimestampColumnWriter() },
-                { DefaultColumnNames.Exception, new ExceptionColumnWriter() },
-                { DefaultColumnNames.LogEventSerialized, new LogEventSerializedColumnWriter() }
+            { DefaultColumnNames.RenderedMessage, new RenderedMessageColumnWriter() },
+            { DefaultColumnNames.MessageTemplate, new MessageTemplateColumnWriter() },
+            { DefaultColumnNames.Level, new LevelColumnWriter() },
+            { DefaultColumnNames.Timestamp, new TimestampColumnWriter() },
+            { DefaultColumnNames.Exception, new ExceptionColumnWriter() },
+            { DefaultColumnNames.LogEventSerialized, new LogEventSerializedColumnWriter() }
         };
 }
